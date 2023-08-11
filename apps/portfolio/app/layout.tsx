2ffc583371a84457f6ba757cabd7cd60c3e15a70
stranger1989 <kkuroda@k2bg.technology>
2023-08-11 10:46:03 +0900

    :sparkles: add font settings

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -1,10 +1,21 @@
+import { Noto_Sans_JP } from "next/font/google";
+
+import "./globals.css";
+
+// If loading a variable font, you don't need to specify the font weight
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-noto-sans-jp",
+});
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja" className={`${notoSansJP.variable}`}>
       <body>{children}</body>
     </html>
   );
