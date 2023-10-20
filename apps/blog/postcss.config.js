dd253d54428d881383e360432b42615b07445ba6
stranger1989 <kkuroda@k2bg.technology>
2023-10-21 08:02:05 +0900

    :package: adopt tailwind

---
+++ apps/blog/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    'tailwindcss/nesting': 'postcss-nesting',
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true,
+      },
+    },
+  },
+};
---
+++ apps/blog/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-unresolved, import/no-extraneous-dependencies
+const sharedConfig = require('tailwind-config');
+
+module.exports = {
+  ...sharedConfig,
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+};
