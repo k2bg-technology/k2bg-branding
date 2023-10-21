82d5ed6e12279a211bd4958832a8ae2d58ca1f7b
stranger1989 <kkuroda@k2bg.technology>
2023-10-21 17:20:51 +0900

    :sparkles: add base section under RootLayout

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,11 +1,34 @@
+import { Noto_Sans_JP } from 'next/font/google';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import './globals.css';
+
+// If loading a variable font, you don't need to specify the font weight
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-noto-sans-jp',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja" className={notoSansJP.variable}>
+      <body>
+        <div className="grid grid-rows-[50px_1fr_300px] h-screen">
+          <Header />
+          <main className="grid grid-cols-12 gap-x-[30px] w-[1140px] px-[15px] mx-auto">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
