8bc85bafde4869c98585dd641e984cddd576a570
stranger1989 <kkuroda@k2bg.technology>
2023-10-28 09:45:42 +0900

    feat: add Avatar component

---
+++ packages/ui/src/Avatar/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Avatar from './Avatar';
+
+const meta: Meta<typeof Avatar> = {
+  component: Avatar,
+  parameters: {},
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    image: (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        alt="Office"
+        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    ),
+    name: 'Kuroda Kentaro',
+  },
+};
---
+++ packages/ui/src/Avatar/Avatar.tsx
@@ -0,0 +1,21 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  image: ReactNode;
+  name?: string;
+}
+
+export default function Avatar({ className, image, name, ...rest }: Props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <div {...rest} className={`inline-flex gap-6 items-center ${className}`}>
+      {image && (
+        <div className="flex flex-none items-center overflow-hidden rounded-full w-12 h-12">
+          {image}
+        </div>
+      )}
+      {name && <span className="text-body-sm font-bold">{name}</span>}
+    </div>
+  );
+}
