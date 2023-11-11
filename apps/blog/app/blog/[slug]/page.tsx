31c5448153261c459d306e8377bae713f7ece4ab
stranger1989 <kkuroda@k2bg.technology>
2023-11-11 20:31:50 +0900

    :sparkles: fetch single-page data and apply it to its layout

--- apps/blog/app/blog/[slug]/page.tsx
+++ apps/blog/app/blog/[slug]/page.tsx
@@ -1,23 +1,22 @@
-import { Client } from '@notionhq/client';
-import { NotionToMarkdown } from 'notion-to-md';
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { Avatar, BlogCard } from 'ui';
 
-const notion = new Client({
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  auth: process.env.NOTION_TOKEN ?? '',
-});
-
-// passing notion client to the option
-const n2m = new NotionToMarkdown({ notionClient: notion });
+import { N2m } from '../../modules/notion/n2m';
+import Notion from '../../modules/notion';
+import Blog from '../../modules/blog';
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: { id: string };
 }) {
-  const mdblocks = await n2m.pageToMarkdown(searchParams.id);
-  const mdString = n2m.toMarkdownString(mdblocks);
+  const notionMarkdownString =
+    await new N2m().fetchNotionPageAndConvertMarkdownString(searchParams.id);
+  const page = new Notion.Page(
+    await new Notion.Fetcher().fetchPage(searchParams.id)
+  );
+  const article = new Blog.Article(page);
 
   return (
     <div className="grid grid-cols-[subgrid] gap-20 col-span-full py-[30px]">
@@ -30,33 +29,35 @@ export default async function Page({
               </a>
             }
             heading={
-              <a href="https://example.com" target="_blank" rel="noreferrer">
-                <h1 className="text-heading-1 font-bold">記事のタイトル</h1>
-              </a>
+              <h1 className="text-heading-1 font-bold">{article.title}</h1>
             }
-            excerpt="記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋記事の抜粋..."
+            excerpt={article.excerpt}
             avatar={
               <Avatar
                 image={
-                  // eslint-disable-next-line @next/next/no-img-element
-                  <img
-                    alt="Office"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  />
+                  <div className="relative w-full h-full">
+                    <Image
+                      alt="author"
+                      src={article.avatar.imageUrl}
+                      className="aspect-square h-full w-full object-cover"
+                      fill
+                    />
+                  </div>
                 }
-                name="Kuroda Kentaro"
+                name={article.avatar.name}
               />
             }
-            date="September 30, 2023"
+            date={article.date}
           />
-          <BlogCard.Media>
-            <a href="https://example.com" target="_blank" rel="noreferrer">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                alt="Office"
-                src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          <BlogCard.Media className="relative w-full h-[37.6rem]">
+            {article.image && (
+              <Image
+                alt="media"
+                src={article.image}
+                className="aspect-square h-full w-full object-cover"
+                fill
               />
-            </a>
+            )}
           </BlogCard.Media>
         </BlogCard>
       </div>
@@ -101,7 +102,7 @@ export default async function Page({
             ),
           }}
         >
-          {mdString.parent}
+          {notionMarkdownString}
         </ReactMarkdown>
       </div>
       <article className="col-start-10 col-end-13 h-[500px] bg-slate-100">
