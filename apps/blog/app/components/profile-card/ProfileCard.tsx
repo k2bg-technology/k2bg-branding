24ca3e7ca505117fb5809503224dfc21787d36e9
stranger1989 <kkuroda@k2bg.technology>
2023-11-26 09:38:09 +0900

    :sparkles: add profile card component

---
+++ apps/blog/app/components/profile-card/ProfileCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import { Avatar } from 'ui';
+
+export default function ProfileCard() {
+  return (
+    <div>
+      <p className="mb-6 text-subtitle-sm font-bold border-b-2 border-b-slate-100">
+        プロフィール
+      </p>
+      <div className="grid gap-5 p-5 rounded-lg bg-base-white/50">
+        <Avatar
+          image={
+            <div className="relative w-full h-full">
+              <Image
+                alt="profile image"
+                src="/me.png"
+                className="aspect-square h-full w-full object-cover"
+                fill
+              />
+            </div>
+          }
+          name="stranger1989"
+        />
+        <div className="grid gap-px">
+          <p className="text-body-sm leading-body-sm text-base-black/80">
+            東京都、世田谷区在住。愛知県出身。
+          </p>
+          <p className="text-body-sm leading-body-sm text-base-black/80">
+            フリーランスとして、Webアプリケーションの開発支援業務をメインに行っています！得意分野はフロントエンド、UI・UX、データ分析です。
+          </p>
+          <p className="text-body-sm leading-body-sm text-base-black/80">
+            当ブログは、「ITエンジニア目線で便利に思うITサービス・アプリの紹介」や、「エンジニアでなくても動かせる簡単なプログラミングを活用したライフハック」など、より多くの人の生活が少しでも快適で便利になる情報発信を目指しています！
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
