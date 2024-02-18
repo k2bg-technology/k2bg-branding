7e6d8be4715edadfe194e4b5eb3278b1046a48fc
stranger1989 <kkuroda@k2bg.technology>
2024-02-18 18:45:29 +0900

    :bug: remove GoogleAdsense component from sidebar

--- apps/blog/app/components/sidebar/Sidebar.tsx
+++ apps/blog/app/components/sidebar/Sidebar.tsx
@@ -4,14 +4,13 @@ import Image from 'next/image';
 import XTimeline from '../x-timeline/XTimeline';
 import InstagramTimeline from '../instagram-timeline/InstagramTimeline';
 import ProfileCard from '../profile-card/ProfileCard';
-import { GoogleAdsense } from '../google-adsense/GoogleAdsense';
 
 export default function Sidebar() {
   return (
     <aside>
       <div className="grid gap-10 w-[25.5rem]">
         {/* eslint-disable-next-line turbo/no-undeclared-env-vars */}
-        {process.env.NODE_ENV === 'development' ? (
+        {process.env.NODE_ENV === 'development' && (
           <div className="relative h-[25.5rem] w-[25.5rem]">
             <Image
               alt="google adsense"
@@ -21,8 +20,6 @@ export default function Sidebar() {
               sizes="100%"
             />
           </div>
-        ) : (
-          <GoogleAdsense />
         )}
         <ProfileCard />
         <XTimeline />
