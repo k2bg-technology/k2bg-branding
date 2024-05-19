dd6f7a1697b2ccc39184288645fde23b6b241db9
stranger1989 <kkuroda@k2bg.technology>
2024-05-19 19:03:19 +0900

    :sparkles:  separate articles component for displaying blog card skelton during fetching data
    The `Articles` component fetches articles and placeholder images, and renders them using the `BlogCard` component.

--- apps/blog/app/category/[category]/page.tsx
+++ apps/blog/app/category/[category]/page.tsx
@@ -1,11 +1,10 @@
 import { Suspense } from 'react';
-import { Avatar, BlogCard } from 'ui';
-import Link from 'next/link';
-import Image from 'next/image';
+import { BlogCard } from 'ui';
 
-import Article from '../../modules/domain/article';
 import Notion from '../../modules/data-access/notion';
 import Pagination from '../../components/pagination/Pagination';
+import { Articles } from '../../components/articles/Articles';
+import Article from '../../modules/domain/article';
 
 export async function generateStaticParams() {
   return ['engineering', 'design', 'data-science', 'life-style'].map(
@@ -68,82 +67,62 @@ export default async function Page({
       )
     );
 
-  const database = await new Notion.Fetcher().fetchDatabase({
-    filter,
-    sorts,
-    page_size: PAGE_SIZE,
-    start_cursor: databasePagination[currentPage - 1],
-  });
+  async function fetchArticles() {
+    const database = await new Notion.Fetcher().fetchDatabase({
+      filter,
+      sorts,
+      page_size: PAGE_SIZE,
+      start_cursor: databasePagination[currentPage - 1],
+    });
 
-  const pages = database.results.map((result) => new Notion.Page(result));
-  const articles = new Article.List(pages);
+    const pages = database.results.map((result) => new Notion.Page(result));
+    const articles = new Article.List(pages);
 
-  const placeHolders = await Article.List.convertImageToPlaceholder(
-    articles.all
-  );
+    const placeHolders = await Article.List.convertImageToPlaceholder(
+      articles.all
+    );
+
+    return {
+      articles,
+      placeHolders,
+    };
+  }
 
   return (
     <>
       <h1 className="col-span-full text-header-1 font-bold capitalize py-4">
         {params.category}
       </h1>
-      <Suspense key={currentPage}>
-        <div className="grid grid-cols-[subgrid] col-span-full py-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 col-span-full gap-12 place-content-start">
-            {articles.all.map((article) => (
-              <BlogCard key={article.title} className="flex-col gap-6">
-                {article.image && (
-                  <Link href={`/blog/${article.slug}`} passHref>
-                    <BlogCard.Media className="relative w-full h-[26.5rem]">
-                      <Image
-                        alt="media"
-                        src={`/api/notion/image/${article.id}`}
-                        className="aspect-square h-full w-full object-cover"
-                        fill
-                        sizes="100%"
-                        placeholder="blur"
-                        blurDataURL={placeHolders[article.id]?.base64}
-                      />
-                    </BlogCard.Media>
-                  </Link>
-                )}
-                <BlogCard.Content
-                  category={
-                    <Link href={`/category/${article.category}`}>
-                      {article.category}
-                    </Link>
-                  }
-                  heading={
-                    <Link href={`/blog/${article.slug}`}>
-                      <h2 className="text-header-2 leading-header-2 font-bold">
-                        {article.title}
-                      </h2>
-                    </Link>
-                  }
-                  excerpt={article.excerpt}
-                  avatar={
-                    article.author && (
-                      <Avatar
-                        image={
-                          <div className="relative w-full h-full">
-                            <Image
-                              alt="author"
-                              src={article.author?.avatar_url ?? ''}
-                              className="aspect-square h-full w-full object-cover"
-                              fill
-                              sizes="100%"
-                            />
-                          </div>
-                        }
-                        name={article.author.name ?? ''}
-                      />
-                    )
-                  }
-                  date={article.releaseDate}
-                />
+      <Suspense
+        key={currentPage}
+        fallback={
+          <div className="grid grid-cols-[subgrid] col-span-full py-12">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 col-span-full gap-12 place-content-start">
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
               </BlogCard>
-            ))}
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
+              </BlogCard>
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
+              </BlogCard>
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
+              </BlogCard>
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
+              </BlogCard>
+              <BlogCard className="flex-col gap-6">
+                <BlogCard.Skelton />
+              </BlogCard>
+            </div>
           </div>
+        }
+      >
+        <div className="grid grid-cols-[subgrid] col-span-full py-12">
+          {/* @ts-expect-error: async component */}
+          <Articles fetchArticles={fetchArticles} />
         </div>
         <div className="flex justify-center grid-cols-[subgrid] col-span-full py-12">
           <Pagination count={Object.keys(databasePagination).length} />
---
+++ apps/blog/app/components/articles/Articles.tsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Avatar, BlogCard } from 'ui';
+import { GetPlaiceholderReturn } from 'plaiceholder';
+
+import Article from '../../modules/domain/article';
+
+interface Props {
+  fetchArticles: () => Promise<{
+    articles: InstanceType<(typeof Article)['List']>;
+    placeHolders: Record<string, GetPlaiceholderReturn>;
+  }>;
+}
+
+export async function Articles(props: Props) {
+  const { fetchArticles } = props;
+
+  const { articles, placeHolders } = await fetchArticles();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 col-span-full gap-12 place-content-start">
+      {articles.all.map((article) => (
+        <BlogCard key={article.title} className="flex-col gap-6">
+          {article.image && (
+            <Link href={`/blog/${article.slug}`} passHref>
+              <BlogCard.Media className="relative w-full h-[26.5rem]">
+                <Image
+                  alt="media"
+                  src={`/api/notion/image/${article.id}`}
+                  className="aspect-square h-full w-full object-cover"
+                  fill
+                  sizes="100%"
+                  placeholder="blur"
+                  blurDataURL={placeHolders[article.id]?.base64}
+                />
+              </BlogCard.Media>
+            </Link>
+          )}
+          <BlogCard.Content
+            category={
+              <Link href={`/category/${article.category}`}>
+                {article.category}
+              </Link>
+            }
+            heading={
+              <Link href={`/blog/${article.slug}`}>
+                <h2 className="text-header-2 leading-header-2 font-bold">
+                  {article.title}
+                </h2>
+              </Link>
+            }
+            excerpt={article.excerpt}
+            avatar={
+              article.author && (
+                <Avatar
+                  image={
+                    <div className="relative w-full h-full">
+                      <Image
+                        alt="author"
+                        src={article.author?.avatar_url ?? ''}
+                        className="aspect-square h-full w-full object-cover"
+                        fill
+                        sizes="100%"
+                      />
+                    </div>
+                  }
+                  name={article.author.name ?? ''}
+                />
+              )
+            }
+            date={article.releaseDate}
+          />
+        </BlogCard>
+      ))}
+    </div>
+  );
+}
