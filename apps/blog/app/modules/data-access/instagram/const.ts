f15a84a6b2beb7ce90b36efd9a661c0c0b31b9e4
stranger1989 <kkuroda@k2bg.technology>
2024-08-05 08:36:48 +0900

    :bulb: Remove unused Instagram access token constant
    This commit removes the unused `INSTAGRAM_LONG_ACCESS_TOKEN` constant from the Instagram data access module. The constant was previously commented out and is no longer needed. This change helps to clean up the codebase and remove unnecessary clutter.

--- apps/blog/app/modules/data-access/instagram/const.ts
+++ apps/blog/app/modules/data-access/instagram/const.ts
@@ -1,4 +1,3 @@
 export const GRAPH_API_BASE_URI = 'https://graph.instagram.com';
 export const INSTAGRAM_LONG_ACCESS_TOKEN =
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
   process.env.INSTAGRAM_LONG_ACCESS_TOKEN || '';
