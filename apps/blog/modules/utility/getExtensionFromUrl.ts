932c8a47ef4029ebb4c3996fe49afc3c65a5eb7c
stranger1989 <kkuroda@k2bg.technology>
2024-11-29 09:35:11 +0900

    ♻️ refactor simplify URL parsing in getExtensionFromUrl function

--- apps/blog/modules/utility/getExtensionFromUrl.ts
+++ apps/blog/modules/utility/getExtensionFromUrl.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export function getExtensionFromUrl(url: string) {
   const parsedUrl = new URL(url);
-  const pathname = parsedUrl.pathname;
+  const { pathname } = parsedUrl;
   const extension = path.parse(pathname).ext;
 
   return extension;
