d171aa47449b89ad30d706ae9084d9c95e64e9a8
stranger1989 <kkuroda@k2bg.technology>
2024-12-08 11:08:59 +0900

    🩹 remove unused eslint-disable comments for environment variable checks in Sidebar and XTimeline components

--- apps/blog/components/sidebar/Sidebar.tsx
+++ apps/blog/components/sidebar/Sidebar.tsx
@@ -9,7 +9,6 @@ export default function Sidebar() {
   return (
     <aside>
       <div className="grid gap-spacious w-[18.375rem]">
-        {/* eslint-disable-next-line turbo/no-undeclared-env-vars */}
         {process.env.NODE_ENV === 'development' && (
           <div className="relative h-[18.375rem]">
             <Image
--- apps/blog/components/x-timeline/const.ts
+++ apps/blog/components/x-timeline/const.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line turbo/no-undeclared-env-vars
 export const X_TIMELINE_URL = process.env.NEXT_PUBLIC_X_TIMELINE_URL || '';
