6cb4db464891f6068dcf77a4ee1a12c689fbee63
stranger1989 <kkuroda@k2bg.technology>
2025-01-05 18:07:21 +0900

    ✨ protect images and posts API by adding API key validation for improved security

--- apps/blog/app/api/images/route.ts
+++ apps/blog/app/api/images/route.ts
@@ -1,8 +1,16 @@
+import { NextRequest } from 'next/server';
+
 import * as UseCases from '../../../modules/use-cases';
 import * as Notion from '../../../modules/data-access/notion';
 import * as Cloudinary from '../../../modules/data-access/cloudinary';
 
-export async function PATCH() {
+export async function PATCH(request: NextRequest) {
+  const apiKey = request.headers.get('x-api-key');
+
+  if (apiKey !== process.env.API_KEY) {
+    return new Response('unauthorized please set x-api-key', { status: 401 });
+  }
+
   const images = await new UseCases.Post.UpsertImages(
     new Notion.Post.Repository(),
     new Notion.Media.Repository(),
--- apps/blog/app/api/posts/route.ts
+++ apps/blog/app/api/posts/route.ts
@@ -1,8 +1,16 @@
+import { type NextRequest } from 'next/server';
+
 import * as UseCases from '../../../modules/use-cases';
 import * as Notion from '../../../modules/data-access/notion';
 import * as Prisma from '../../../modules/data-access/prisma';
 
-export async function PATCH() {
+export async function PATCH(request: NextRequest) {
+  const apiKey = request.headers.get('x-api-key');
+
+  if (apiKey !== process.env.API_KEY) {
+    return new Response('unauthorized please set x-api-key', { status: 401 });
+  }
+
   const posts = await new UseCases.Post.UpsertPosts(
     new Notion.Post.Repository(),
     new Prisma.Post.Repository()
