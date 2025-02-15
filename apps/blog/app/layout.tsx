b3ad6940db2ad0f245d30bf3f9c2074e0cc20a29
stranger1989 <kkuroda@k2bg.technology>
2025-02-15 18:41:24 +0900

    ✨ integrate ScrollArea component into layout and Drawer for enhanced scrolling experience

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Noto_Sans_JP } from 'next/font/google';
+import { ScrollArea } from 'ui';
 
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
@@ -22,13 +23,14 @@ export default function RootLayout({
   return (
     <html lang="ja" className={notoSansJP.variable}>
       <body>
-        <div className="grid grid-rows-[3rem_1fr_18.75rem] grid-cols-[1fr_calc(100%-2rem)_1fr] md:grid-cols-[1fr_46rem_1fr] xl:grid-cols-[1fr_78rem_1fr] h-screen">
+        <ScrollArea className="h-screen">
           <Header />
           <main className="col-start-2 -col-end-2 grid grid-cols-4 md:grid-cols-6 xl:grid-cols-12 gap-x-6 gap-y-12 auto-rows-max py-12">
             {children}
           </main>
           <Footer />
         </div>
+        </ScrollArea>
       </body>
       <GoogleAdsense />
     </html>
--- packages/ui/src/components/Drawer/Drawer.tsx
+++ packages/ui/src/components/Drawer/Drawer.tsx
@@ -6,6 +6,7 @@ import * as RadixDialog from '@radix-ui/react-dialog';
 import { Icon } from '../Icon';
 import { Button } from '../Button';
 import { twMerge } from '../../utils/extendTailwindMerge';
+import { ScrollArea } from '../ScrollArea';
 
 import styles from './Drawer.module.css';
 
@@ -32,22 +33,24 @@ export default function Drawer({
         <RadixDialog.Content
           {...rest}
           className={twMerge(
-            'grid auto-rows-max gap-5 fixed top-0 right-0 w-max h-full rounded-xl p-6 pt-14 bg-white drop-shadow-xl overflow-y-auto',
+            'grid auto-rows-max gap-5 fixed top-0 right-0 w-max h-full rounded-xl bg-white drop-shadow-xl',
             styles.Content,
             className
           )}
         >
-          {title && (
-            <RadixDialog.Title className="text-body-r-sm leading-body-r-sm font-bold">
-              {title}
-            </RadixDialog.Title>
-          )}
-          {description && (
-            <RadixDialog.Description className="text-body-r-sm leading-body-r-sm">
-              {description}
-            </RadixDialog.Description>
-          )}
-          <div className="h-full">{mainContent}</div>
+          <ScrollArea className="p-6 pt-14 h-screen">
+            {title && (
+              <RadixDialog.Title className="text-body-r-sm leading-body-r-sm font-bold">
+                {title}
+              </RadixDialog.Title>
+            )}
+            {description && (
+              <RadixDialog.Description className="text-body-r-sm leading-body-r-sm">
+                {description}
+              </RadixDialog.Description>
+            )}
+            {mainContent}
+          </ScrollArea>
           <RadixDialog.Close asChild>
             <Button
               type="button"
