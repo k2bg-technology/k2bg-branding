6b4232ef79487f26fc6f1d751f09a2c48b488e59
stranger1989 <kkuroda@k2bg.technology>
2025-02-22 16:21:53 +0900

    💄 refactor Sidebar component for improved layout and sticky ad positioning

--- apps/blog/components/sidebar/Sidebar.tsx
+++ apps/blog/components/sidebar/Sidebar.tsx
@@ -8,24 +8,26 @@ import SideBarAd from '../google-adsense/SideBarAd';
 
 export default function Sidebar() {
   return (
-    <aside>
-      <div className="grid gap-spacious max-w-[18.125rem]">
-        {process.env.NODE_ENV === 'production' ? (
-          <SideBarAd />
-        ) : (
-          <div className="relative h-[18.125rem]">
-            <Image
-              alt="google adsense"
-              src="/adsense_dummy.png"
-              className="aspect-square object-cover"
-              fill
-              sizes="100%"
-            />
-          </div>
-        )}
+    <aside className="contents">
+      <div className="grid auto-rows-max gap-spacious max-w-[18.125rem] h-full">
         <ProfileCard />
         <XTimeline />
         <InstagramTimeline />
+        <div className="self-start sticky top-[5rem]">
+          {process.env.NODE_ENV === 'production' ? (
+            <SideBarAd />
+          ) : (
+            <div className="h-[18.125rem]">
+              <Image
+                alt="google adsense"
+                src="/adsense_dummy.png"
+                className="aspect-square object-cover"
+                fill
+                sizes="100%"
+              />
+            </div>
+          )}
+        </div>
       </div>
     </aside>
   );
