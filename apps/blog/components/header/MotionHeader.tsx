30afe3f1049fe4b053f4e9e73505bb8aa55dd33d
stranger1989 <kkuroda@k2bg.technology>
2025-03-16 11:36:25 +0900

    ✨ refactor Header component to use MotionHeader for scroll-based visibility and motion effects

---
+++ apps/blog/components/header/MotionHeader.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { motion, useScroll } from 'motion/react';
+
+import { usePageScrollAreaStore } from '../page-scroll-area/PageScrollArea';
+
+type Props = React.ComponentPropsWithoutRef<'header'>;
+
+export default function MotionHeader(props: Props) {
+  const { children } = props;
+
+  const ref = usePageScrollAreaStore((state) => state.ref);
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const { scrollY } = useScroll({ container: ref ?? undefined });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = scrollY.get();
+      setIsVisible(currentScrollY < lastScrollY || currentScrollY < 50);
+      setLastScrollY(currentScrollY);
+    };
+
+    const unsubscribe = scrollY.on('change', handleScroll);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [ref, scrollY, lastScrollY]);
+
+  return (
+    <motion.header
+      className="order-first sticky top-0 backdrop-blur-xs col-span-full grid grid-cols-[subgrid] bg-base-white/50 drop-shadow-xs"
+      initial={{ y: 0 }}
+      animate={{ y: isVisible ? 0 : '-100%' }}
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.header>
+  );
+}
