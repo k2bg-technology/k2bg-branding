3d3b0077edb263637593c42cd615ec5496dc97e3
stranger1989 <kkuroda@k2bg.technology>
2025-03-22 09:36:47 +0900

    ✨ add SidebarItem component for structured sidebar content display

---
+++ apps/blog/components/sidebar/SidebarItem.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  title: string;
+}
+
+export default function SidebarItem(props: React.PropsWithChildren<Props>) {
+  const { title, children } = props;
+
+  return (
+    <div>
+      <p className="mb-6 text-subtitle-sm font-bold border-b-2 border-b-slate-100">
+        {title}
+      </p>
+      {children}
+    </div>
+  );
+}
