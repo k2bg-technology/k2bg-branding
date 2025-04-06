987a73ecb9578feadc1b14baf055121cedc86e5c
stranger1989 <kkuroda@k2bg.technology>
2025-04-06 09:52:23 +0900

    ✨ add generateHtmlTemplate utility for rendering HTML templates with Handlebars

---
+++ apps/blog/modules/utility/generateHtmlTemplate.ts
@@ -0,0 +1,15 @@
+import fs, { PathOrFileDescriptor } from 'fs';
+
+import handlebars from 'handlebars';
+
+export function generateHtmlTemplate(
+  /** Path to the HTML template file */
+  filePath: PathOrFileDescriptor,
+  /** Data to inject into the template as key-value pairs */
+  templateContext: Record<string, unknown>
+) {
+  const file = fs.readFileSync(filePath, 'utf-8');
+  const template = handlebars.compile(file);
+
+  return template(templateContext);
+}
