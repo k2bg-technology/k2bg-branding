3b120acbebb9101806192096e6f8b39624bbd471
stranger1989 <kkuroda@k2bg.technology>
2025-04-07 09:32:28 +0900

    ✨ add HCaptcha integration for enhanced form validation and security

--- apps/blog/app/contact/ContactForm.tsx
+++ apps/blog/app/contact/ContactForm.tsx
@@ -4,12 +4,17 @@ import { Button, Form } from 'ui';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 import { contactSchema } from '../../modules/interfaces/contact/validator';
 
 import { contactFormAction } from './contactFormAction';
 
 export default function ContactForm() {
+  const captchaRef = useRef<HCaptcha>(null);
+  const [token, setToken] = useState<string | null>(null);
+
   const defaultValues = {
     name: '',
     email: '',
@@ -36,11 +41,31 @@ export default function ContactForm() {
     mode: 'onBlur',
   });
 
+  function onExpire() {
+    setToken(null);
+  }
+
+  function onError() {
+    setToken(null);
+  }
+
   return (
     <form
       noValidate
       className="flex flex-col gap-spacious h-full md:gap-spacious"
-      onSubmit={handleSubmit((data) => mutation.mutate(data))}
+      onSubmit={handleSubmit((visitor) => {
+        if (!token) {
+          // eslint-disable-next-line no-alert
+          alert('認証を完了してください。');
+
+          return;
+        }
+
+        mutation.mutate({
+          ...visitor,
+          token: token || '',
+        });
+      })}
     >
       <div className="flex flex-col gap-6 md:flex-row">
         <div className="flex flex-col gap-2 w-full">
@@ -113,6 +138,13 @@ export default function ContactForm() {
           )}
         </Form.Control>
       </div>
+      <HCaptcha
+        ref={captchaRef}
+        sitekey={process.env.NEXT_PUBLIC_H_CAPTCHA_SITE_KEY || ''}
+        onVerify={setToken}
+        onExpire={() => onExpire()}
+        onError={() => onError()}
+      />
       <ul className="flex flex-col gap-spacious md:flex-row">
         <li>
           <Button
--- apps/blog/app/contact/contactFormAction.ts
+++ apps/blog/app/contact/contactFormAction.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { verify } from 'hcaptcha';
+
 import * as SendGrid from '../../modules/data-access/send-grid';
 import {
   Contact,
@@ -7,13 +9,28 @@ import {
 } from '../../modules/interfaces/contact/validator';
 import { SendEmail } from '../../modules/use-cases/contact';
 
-export async function contactFormAction(data: Contact) {
-  const validatedFields = contactSchema.safeParse(data);
+export async function contactFormAction(
+  data: Contact & {
+    token: string;
+  }
+) {
+  const { token, ...visitor } = data;
+
+  const validatedFields = contactSchema.safeParse(visitor);
 
   if (!validatedFields.success) {
     throw new Error(
       JSON.stringify(validatedFields.error.flatten().fieldErrors)
     );
+  }
+
+  const verifyResponse = await verify(
+    process.env.H_CAPTCHA_SECRET || '',
+    token
+  );
+
+  if (!verifyResponse.success) {
+    throw new Error('captcha verification failed');
   }
 
   await new SendEmail(new SendGrid.Adapter()).execute(validatedFields.data);
