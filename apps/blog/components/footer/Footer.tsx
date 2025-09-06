141c635b373cf69059705afebaa64685b7a7c430
stranger1989 <kkuroda@k2bg.technology>
2025-09-06 20:11:12 +0900

    💅 style: Align copyright text to the left for improved readability

--- apps/blog/components/footer/Footer.tsx
+++ apps/blog/components/footer/Footer.tsx
@@ -127,7 +127,7 @@ export default function Footer() {
           </div>
 
           <div className="flex items-center justify-between">
-            <p className="mt-4 text-center text-body-r-sm text-white lg:mt-0 lg:text-right">
+            <p className="mt-4 text-left text-body-r-sm text-white lg:mt-0">
               {`Copyright © ${format(
                 new Date(),
                 'yyyy'
