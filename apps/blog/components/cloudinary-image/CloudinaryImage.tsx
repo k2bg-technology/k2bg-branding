1b669e5805bdb269ea19ef49818658c4bc426adf
stranger1989 <kkuroda@k2bg.technology>
2025-12-20 21:01:01 +0900

    🐛 Fix: Enhance error handling and structure in CloudinaryImage component
    * workaround

--- apps/blog/components/cloudinary-image/CloudinaryImage.tsx
+++ apps/blog/components/cloudinary-image/CloudinaryImage.tsx
@@ -1,4 +1,5 @@
 import Image, { ImageProps } from 'next/image';
+import { Icon, Skelton } from 'ui';
 
 import * as Cloudinary from '../../modules/data-access/cloudinary';
 
@@ -12,30 +13,45 @@ export async function CloudinaryImage(props: CloudinaryImageProps) {
 
   const imageRepository = new Cloudinary.Repository();
 
-  const version = await imageRepository.fetchImageVersion(publicId);
-  const optimizedImageUrl = imageRepository.getOptimizedImageUrl(
-    publicId,
-    version
-  );
-  const placeholderImageUrl = imageRepository.getPlaceholderImageUrl(
-    publicId,
-    version
-  );
-  const placeholderImageBase64 = await fetch(placeholderImageUrl).then(
-    async (res) =>
-      `data:image/webp;base64,${Buffer.from(await res.arrayBuffer()).toString(
-        'base64'
-      )}`
-  );
+  try {
+    const version = await imageRepository.fetchImageVersion(publicId);
+    const optimizedImageUrl = imageRepository.getOptimizedImageUrl(
+      publicId,
+      version
+    );
+    const placeholderImageUrl = imageRepository.getPlaceholderImageUrl(
+      publicId,
+      version
+    );
+    const placeholderImageBase64 = await fetch(placeholderImageUrl).then(
+      async (res) =>
+        `data:image/webp;base64,${Buffer.from(await res.arrayBuffer()).toString(
+          'base64'
+        )}`
+    );
 
-  return (
-    <Image
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...rest}
-      src={optimizedImageUrl}
-      alt={alt}
-      placeholder="blur"
-      blurDataURL={placeholderImageBase64}
-    />
-  );
+    return (
+      <Image
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...rest}
+        src={optimizedImageUrl}
+        alt={alt}
+        placeholder="blur"
+        blurDataURL={placeholderImageBase64}
+      />
+    );
+  } catch {
+    return (
+      <Skelton className="w-full h-full">
+        <Skelton.Box className="w-full h-full">
+          <Icon
+            name="photo"
+            color="var(--color-base-white)"
+            width={30}
+            height={30}
+          />
+        </Skelton.Box>
+      </Skelton>
+    );
+  }
 }
