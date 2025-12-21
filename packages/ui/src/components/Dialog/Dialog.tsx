366524c4517559fdaea200e42530dfa943ed9a7b
stranger1989 <kkuroda@k2bg.technology>
2025-12-22 08:34:22 +0900

    💄 Fix: Correct className string formatting in Dialog component

--- packages/ui/src/components/Dialog/Dialog.tsx
+++ packages/ui/src/components/Dialog/Dialog.tsx
@@ -29,7 +29,7 @@ export default function Dialog({
         <RadixDialog.Content className="flex flex-col gap-6 fixed top-[50%] left-[50%] w-max max-w-[calc(100%-2rem)] h-max max-h-[calc(100%-4rem)] translate-x-[-50%] translate-y-[-50%] rounded-xl py-6 p-normal bg-white focus:outline-hidden md:p-6">
           <RadixDialog.Title
             className={twMerge(
-              '"text-body-r-sm leading-body-r-sm font-bold"',
+              'text-body-r-sm leading-body-r-sm font-bold',
               !title && 'hidden'
             )}
           >
