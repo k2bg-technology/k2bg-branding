8fde9c09174075e0cec3d82b8638574224fa25c9
stranger1989 <kkuroda@k2bg.technology>
2026-01-11 08:25:29 +0900

    🧑‍💻 Remove ESLint and Prettier in favor of Biome
    Consolidates linting and formatting tooling by fully migrating to Biome.
    
    Removes ESLint configuration files, custom ESLint config package,
    Prettier config, and related dependencies from all apps and packages.
    
    Also removes the eslint.ignoreDuringBuilds option from Next.js configs
    since ESLint is no longer used in the build process.

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['ui', 'tailwind-config'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg/,
