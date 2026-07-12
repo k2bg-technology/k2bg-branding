8517e4ea2292fc7741bc4dc3f48183a67729d2e2
krd-knt <kkuroda@k2bg.technology>
2026-07-12 10:35:32 +0900

    ✨ Feat: #382 Add VisualShowcase pattern

--- apps/scene-studio/src/Root.tsx
+++ apps/scene-studio/src/Root.tsx
@@ -1,17 +1,25 @@
 import { Composition, Folder } from 'remotion';
-
+import visualShowcaseSample from '../data/visual-showcase.sample.json';
 import { BrandDemo } from './compositions/BrandDemo';
 import { getBrandDemoDurationInFrames } from './compositions/BrandDemo/scenes';
 import {
   PRIMITIVE_DEMO_DURATION_IN_FRAMES,
   primitiveDemos,
 } from './compositions/primitives/demos';
+import { VisualShowcase } from './patterns/VisualShowcase';
+import {
+  calculateVisualShowcaseMetadata,
+  getVisualShowcaseDurationInFrames,
+} from './patterns/VisualShowcase/timeline';
+import { visualShowcaseSchema } from './schemas/visualShowcase';
 
 import './style.css';
 
 const SCENE_WIDTH = 1080;
 const SCENE_HEIGHT = 1920;
 const SCENE_FPS = 30;
+const visualShowcaseDefaultProps =
+  visualShowcaseSchema.parse(visualShowcaseSample);
 
 export function RemotionRoot() {
   return (
@@ -38,6 +46,21 @@ export function RemotionRoot() {
             durationInFrames={PRIMITIVE_DEMO_DURATION_IN_FRAMES}
           />
         ))}
+      </Folder>
+      <Folder name="patterns">
+        <Composition
+          id="visual-showcase"
+          component={VisualShowcase}
+          width={SCENE_WIDTH}
+          height={SCENE_HEIGHT}
+          fps={SCENE_FPS}
+          durationInFrames={getVisualShowcaseDurationInFrames(
+            visualShowcaseDefaultProps
+          )}
+          defaultProps={visualShowcaseDefaultProps}
+          schema={visualShowcaseSchema}
+          calculateMetadata={calculateVisualShowcaseMetadata}
+        />
       </Folder>
     </>
   );
